1aa8ef3d551e804f0d5793fe916dd5d9ea8eb0d2
rofiqr <piqsworkmail@gmail.com>
2026-03-06 02:31:12 +0700

    Upload web anniversary

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* Konfigurasi untuk Deploy ke GitHub Pages */
-  output: 'export', // Mengubah Next.js menjadi mode statis (HTML/CSS/JS biasa)
+  output: 'export', 
   
-  // Ganti 'our-anniversary' dengan nama asli repository GitHub kamu
+  // Pastikan ini sesuai dengan nama repository GitHub kamu
   basePath: '/our-anniversary', 
   
-  // Diperlukan agar gambar (foto-foto kamu) bisa muncul tanpa error di GitHub Pages
   images: {
     unoptimized: true,
   },
   
-  // Opsi bawaan kamu
-  experimental: {
-    reactCompiler: true,
-  }
+  // Penulisan yang benar untuk Next.js 16
+  reactCompiler: true, 
 };
 
 export default nextConfig;
